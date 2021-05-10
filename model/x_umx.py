40b6f236a88e7ccc78c63bab5328c2b34344bc8d
Chin Yun Yu <ya70201@gmail.com>
2021-05-10 22:10:26 +0800

    roll back to original version

--- model/x_umx.py
+++ model/x_umx.py
@@ -25,10 +25,10 @@ class X_UMX(nn.Module):
         self.nb_layers = nb_layers
 
         self.input_means = nn.Parameter(torch.zeros(4 * self.max_bins))
-        # self.input_scale = nn.Parameter(torch.ones(4 * self.max_bins))
+        self.input_scale = nn.Parameter(torch.ones(4 * self.max_bins))
 
-        # self.output_means = nn.Parameter(torch.zeros(4 * self.nb_output_bins))
-        # self.output_scale = nn.Parameter(torch.ones(4 * self.nb_output_bins))
+        self.output_means = nn.Parameter(torch.zeros(4 * self.nb_output_bins))
+        self.output_scale = nn.Parameter(torch.ones(4 * self.nb_output_bins))
 
         self.affine1 = nn.Sequential(
             nn.Conv1d(nb_channels * self.max_bins * 4,
@@ -54,7 +54,7 @@ class X_UMX(nn.Module):
             num_layers=nb_layers,
             dropout=0.4,
             bidirectional=True)
-        self.others_lstm = nn.LSTM(
+        self.other_lstm = nn.LSTM(
             input_size=self.hidden_channels,
             hidden_size=self.hidden_channels // 2,
             num_layers=nb_layers,
@@ -75,7 +75,8 @@ class X_UMX(nn.Module):
         batch, channels, bins, frames = spec.shape
         spec = spec[..., :self.max_bins, :]
 
-        x = (spec.unsqueeze(1) - self.input_means.view(4, 1, -1, 1))
+        x = (spec.unsqueeze(1) + self.input_means.view(4, 1, -1, 1)) * \
+            self.input_scale.view(4, 1, -1, 1)
 
         x = x.view(batch, -1, frames)
         cross_1 = self.affine1(x).view(batch, 4, -1, frames).mean(1)
@@ -83,15 +84,16 @@ class X_UMX(nn.Module):
         cross_1 = cross_1.permute(2, 0, 1)
         bass, *_ = self.bass_lstm(cross_1)
         drums, *_ = self.drums_lstm(cross_1)
-        others, *_ = self.others_lstm(cross_1)
+        others, *_ = self.other_lstm(cross_1)
         vocals, *_ = self.vocals_lstm(cross_1)
 
         avg = (bass + drums + vocals + others) * 0.25
         cross_2 = torch.cat([cross_1, avg], 2).permute(1, 2, 0)
 
-        mask = F.softmax(self.affine2(cross_2).view(
-            batch, 4, channels, bins, frames), 1)
-        return mask
+        mask = self.affine2(cross_2).view(batch, 4, channels, bins, frames) * \
+            self.output_scale.view(4, 1, -1, 1) + \
+            self.output_means.view(4, 1, -1, 1)
+        return mask.relu()
 
 
 if __name__ == "__main__":
