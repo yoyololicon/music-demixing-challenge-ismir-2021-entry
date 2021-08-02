1d6401161b0619c3f8cac821cc3923da43c48464
Chin Yun Yu <ya70201@gmail.com>
2021-08-02 14:38:09 +0800

    apply changes of final model

--- model/unet_att.py
+++ model/unet_att.py
@@ -136,7 +136,7 @@ class MultiHeadAttention(nn.Module):
             unfold_q.shape[0], -1, k_depth_per_head)
 
         bias = (unfold_k.abs().sum(-2, keepdim=True)
-                == 0).to(unfold_k.dtype) * -1e-9
+                == 0).to(unfold_k.dtype) * -1e9
 
         logits = unfold_q @ unfold_k + bias
         weights = logits.softmax(-1)
@@ -218,7 +218,7 @@ class UNetAttn(nn.Module):
                                concat_channels, 5, 2, 2, bias=False),
             nn.BatchNorm2d(concat_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(concat_channels, 2, 3, padding=2, dilation=2)
+            nn.Conv2d(concat_channels, 8, 3, padding=2, dilation=2)
         )
 
     def forward(self, spec):
@@ -246,6 +246,7 @@ class UNetAttn(nn.Module):
 
         x = F.pad(x, [0, spec.shape[3] - x.shape[3], 0,
                       spec.shape[2] - x.shape[2]], value=0.25)
+        x = x.view(x.shape[0], 4, 2, x.shape[2], x.shape[3])
         return x
 
 
